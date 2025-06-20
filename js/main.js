956ddb03219bd8899bdac0ccf2bc1a5ed58f1e8e
Liz <nrdygrl24.0@gmail.com>
2025-06-19 23:28:48 -0400

    Added a done arrow function after it's done searching for the gif it can display the gif and the title on the website

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ $(document).ready(() => {
         let userInput = $('#searchInput').val();
         alert(userInput);
          $.ajax({
-        url: 'https://api.giphy.com'
+        url: 'https://api.giphy.com/${userInput}',
+        })
+        .done((res)=> {
+            let gifs = res.Search;
+            $.each(gifs, (i, e) => {
+                let gif = e.gif;
+                console.log("gif", gif);
+                $("body").append(`<img src="${gif}" alt="gif"/>`)
+            })
+            console.log("res", res.Search)
         })
     });
 });
